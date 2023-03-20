9bee0e16cd30b6a5a985575c7b99694d3051f0b3
Jishuna <joshl5324@gmail.com>
2023-03-20 12:49:16 -0400

    Localization

---
+++ jishlib-core/src/main/java/me/jishuna/jishlib/Localization.java
@@ -0,0 +1,34 @@
+package me.jishuna.jishlib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+
+public class Localization {
+	private static final Localization instance = new Localization();
+	private final Map<String, String> localizationMap = new HashMap<>();
+
+	public String localize(String key) {
+		return this.localizationMap.getOrDefault(key, "Missing key: " + key);
+	}
+
+	public String localize(String key, Object... format) {
+		return String.format(localize(key), format);
+	}
+
+	public void parse(YamlConfiguration config) {
+		this.localizationMap.clear();
+		for (String key : config.getKeys(true)) {
+			if (!config.isString(key))
+				continue;
+
+			this.localizationMap.put(key, StringUtils.parseToLegacy(config.getString(key)));
+		}
+	}
+
+	public static Localization getInstance() {
+		return instance;
+	}
+
+}
