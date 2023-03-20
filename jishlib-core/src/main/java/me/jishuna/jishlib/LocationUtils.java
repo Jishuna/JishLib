fbf0f8cefe44b72b9292ed6b32bc5d080d9dbf68
Jishuna <joshl5324@gmail.com>
2023-03-20 12:10:13 -0400

    centerLocation

---
+++ jishlib-core/src/main/java/me/jishuna/jishlib/LocationUtils.java
@@ -0,0 +1,21 @@
+package me.jishuna.jishlib;
+
+import org.bukkit.Location;
+
+public class LocationUtils {
+	private LocationUtils() {
+	}
+
+	public static Location centerLocation(Location location) {
+		return centerLocation(location, true);
+	}
+
+	public static Location centerLocation(Location location, boolean vertical) {
+		double x = location.getBlockX() + 0.5;
+		double y = location.getBlockY() + (vertical ? 0.5 : 0);
+		double z = location.getBlockZ() + 0.5;
+
+		return new Location(location.getWorld(), x, y, z);
+	}
+
+}
