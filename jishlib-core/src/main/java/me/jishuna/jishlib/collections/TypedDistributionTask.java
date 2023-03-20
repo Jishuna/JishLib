243c769b7dbba03447cd4f0ebf3b5383fe67e3bb
Jishuna <joshl5324@gmail.com>
2023-03-20 13:09:17 -0400

    Work distributon

---
+++ jishlib-core/src/main/java/me/jishuna/jishlib/collections/TypedDistributionTask.java
@@ -0,0 +1,61 @@
+package me.jishuna.jishlib.collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class TypedDistributionTask<T> implements Runnable {
+
+	protected final Consumer<T> action;
+	protected final Predicate<T> escapeCondition;
+	protected final List<LinkedList<T>> valueMatrix;
+	protected final int distributionSize;
+	protected int currentPosition = 0;
+
+	public TypedDistributionTask(Consumer<T> action, Predicate<T> escapeCondition, int distributionSize) {
+		this.distributionSize = distributionSize;
+		this.action = action;
+		this.escapeCondition = escapeCondition;
+		this.valueMatrix = new ArrayList<>(distributionSize);
+
+		for (int i = 0; i < distributionSize; i++) {
+			this.valueMatrix.add(new LinkedList<>());
+		}
+	}
+
+	public void addValue(T value) {
+		List<T> smallest = this.valueMatrix.get(0);
+		if (!smallest.isEmpty()) {
+			for (int index = 0; index < this.distributionSize; index++) {
+				List<T> next = this.valueMatrix.get(index);
+				if (next.size() < smallest.size()) {
+					smallest = next;
+				}
+			}
+		}
+		smallest.add(value);
+	}
+
+	protected void incrementPosition() {
+		if (++this.currentPosition == this.distributionSize) {
+			this.currentPosition = 0;
+		}
+	}
+
+	@Override
+	public void run() {
+		this.valueMatrix.get(this.currentPosition).removeIf(this::checkThenExecute);
+		this.incrementPosition();
+	}
+
+	protected boolean checkThenExecute(T value) {
+		if (this.escapeCondition.test(value)) {
+			return true;
+		}
+		this.action.accept(value);
+		return false;
+	}
+
+}
