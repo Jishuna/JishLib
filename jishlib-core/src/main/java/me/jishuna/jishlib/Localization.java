1a9f152108f04298953fdb9ab860aae7d665c728
Jishuna <joshl5324@gmail.com>
2023-04-23 16:31:48 -0400

    Combine localization

--- jishlib-core/src/main/java/me/jishuna/jishlib/Localization.java
+++ jishlib-core/src/main/java/me/jishuna/jishlib/Localization.java
@@ -3,13 +3,21 @@ package me.jishuna.jishlib;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent.Builder;
+import net.kyori.adventure.text.TextReplacementConfig;
+
 public class Localization {
+	private static final Pattern PATTERN = Pattern.compile("%[^%]*%");
 	private static final Localization instance = new Localization();
 
 	private final Map<String, String> localizationMap = new HashMap<>();
+	private final Map<Component, Component> componentMap = new HashMap<>();
 	private YamlConfiguration config;
 
 	public String localize(String key) {
@@ -21,9 +29,20 @@ public class Localization {
 		return MessageFormat.format(localize(key), format);
 	}
 
+	public Component localizeComponent(Component component) {
+		return componentMap.computeIfAbsent(component, key -> component.replaceText(
+				TextReplacementConfig.builder().match(PATTERN).replacement(this::replacePlaceholders).build()));
+	}
+
+	private Component replacePlaceholders(MatchResult result, Builder builder) {
+		String key = result.group().replace("%", "");
+		return StringUtils.parseComponent(config.getString(key, "Missing key: " + key));
+	}
+
 	public void setConfig(YamlConfiguration config) {
 		this.config = config;
 		this.localizationMap.clear();
+		this.componentMap.clear();
 	}
 
 	public static Localization getInstance() {
