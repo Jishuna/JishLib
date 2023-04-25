eb96e23602546bde66cf856a6c1d0befb003dc04
Jishuna <joshl5324@gmail.com>
2023-04-25 19:04:10 -0400

    Clamp methods

--- jishlib-core/src/main/java/me/jishuna/jishlib/Utils.java
+++ jishlib-core/src/main/java/me/jishuna/jishlib/Utils.java
@@ -3,14 +3,34 @@ package me.jishuna.jishlib;
 import org.bukkit.Chunk;
 
 public class Utils {
-	private Utils() {
-	}
+    private Utils() {
+    }
 
-	public static boolean isWithinBounds(int value, int min, int max) {
-		return value >= min && value <= max;
-	}
+    public static boolean isWithinBounds(int value, int min, int max) {
+        return value >= min && value <= max;
+    }
 
-	public static long getChunkKey(Chunk chunk) {
-		return ((long) chunk.getZ() << 32) | (chunk.getX() & 0xFFFFFFFFL);
-	}
+    public static long getChunkKey(Chunk chunk) {
+        return ((long) chunk.getZ() << 32) | (chunk.getX() & 0xFFFFFFFFL);
+    }
+
+    public static long clamp(long value, long min, long max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    public static int clamp(int value, int min, int max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    public static short clamp(short value, short min, short max) {
+        return (short) Math.min(Math.max(value, min), max);
+    }
+
+    public static double clamp(double value, double min, double max) {
+        return Math.min(Math.max(value, min), max);
+    }
+
+    public static float clamp(float value, float min, float max) {
+        return Math.min(Math.max(value, min), max);
+    }
 }
