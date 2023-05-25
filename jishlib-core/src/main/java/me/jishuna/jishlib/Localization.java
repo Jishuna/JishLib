84e2e601d8073a15fafab0af958e025d058a1f57
Jishuna <joshl5324@gmail.com>
2023-05-25 18:44:22 -0400

    Localize a list of values

--- jishlib-core/src/main/java/me/jishuna/jishlib/Localization.java
+++ jishlib-core/src/main/java/me/jishuna/jishlib/Localization.java
@@ -1,7 +1,9 @@
 package me.jishuna.jishlib;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
@@ -13,40 +15,51 @@ import net.kyori.adventure.text.TextComponent.Builder;
 import net.kyori.adventure.text.TextReplacementConfig;
 
 public class Localization {
-	private static final Pattern PATTERN = Pattern.compile("%[^%]*%");
-	private static final Localization instance = new Localization();
+    private static final Pattern PATTERN = Pattern.compile("%[^%]*%");
+    private static final Localization instance = new Localization();
 
-	private final Map<String, String> localizationMap = new HashMap<>();
-	private final Map<Component, Component> componentMap = new HashMap<>();
-	private YamlConfiguration config;
+    private final Map<String, String> localizationMap = new HashMap<>();
+    private final Map<Component, Component> componentMap = new HashMap<>();
+    private YamlConfiguration config;
 
-	public String localize(String key) {
-		return this.localizationMap.computeIfAbsent(key,
-				mapKey -> StringUtils.parseToLegacy(config.getString(mapKey, "Missing key: " + mapKey)));
-	}
+    public String localize(String key) {
+        return this.localizationMap.computeIfAbsent(key, mapKey -> StringUtils.miniMessageToLegacy(config.getString(mapKey, "Missing key: " + mapKey)));
+    }
 
-	public String localize(String key, Object... format) {
-		return MessageFormat.format(localize(key), format);
-	}
+    public String localize(String key, Object... format) {
+        return MessageFormat.format(localize(key), format);
+    }
 
-	public Component localizeComponent(Component component) {
-		return componentMap.computeIfAbsent(component, key -> component.replaceText(
-				TextReplacementConfig.builder().match(PATTERN).replacement(this::replacePlaceholders).build()));
-	}
+    public List<String> localizeAll(String baseKey) {
+        List<String> strings = new ArrayList<>();
+        int index = 1;
+        String key = baseKey + "-" + index;
 
-	private Component replacePlaceholders(MatchResult result, Builder builder) {
-		String key = result.group().replace("%", "");
-		return StringUtils.parseComponent(config.getString(key, "Missing key: " + key));
-	}
+        while (this.config.isSet(key)) {
+            strings.add(localize(key));
+            key = baseKey + "-" + ++index;
+        }
+        
+        return strings;
+    }
 
-	public void setConfig(YamlConfiguration config) {
-		this.config = config;
-		this.localizationMap.clear();
-		this.componentMap.clear();
-	}
+    public Component localizeComponent(Component component) {
+        return componentMap.computeIfAbsent(component, key -> component.replaceText(TextReplacementConfig.builder().match(PATTERN).replacement(this::replacePlaceholders).build()));
+    }
 
-	public static Localization getInstance() {
-		return instance;
-	}
+    private Component replacePlaceholders(MatchResult result, Builder builder) {
+        String key = result.group().replace("%", "");
+        return StringUtils.fromMiniMessage(config.getString(key, "Missing key: " + key));
+    }
+
+    public void setConfig(YamlConfiguration config) {
+        this.config = config;
+        this.localizationMap.clear();
+        this.componentMap.clear();
+    }
+
+    public static Localization getInstance() {
+        return instance;
+    }
 
 }
